8b4ce724cba270a2d70601fdba61ddbca6a68d3f
dasha <edinad@mer.ci.nsu.ru>
2022-12-11 16:35:06 +0700

    alert про волка

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Alert} from "./components/Alert";
+import './App.css';
+
+function App() {
+  return <div><Alert text={"Заяц украл зайчат"} delay={5}></Alert></div>
+}
+
+export default App;
---
+++ src/components/Alert.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useState, useEffect } from 'react'
+import styles from './Alert.module.css';
+
+type AlertProps = {
+    text: string;
+    delay: number;
+}
+
+export function Alert({text, delay}: AlertProps) {
+    const [show, setShow] = useState(true)
+
+    useEffect(() => {
+        const timeId = setTimeout(() => {
+            // After 3 seconds set the show value to false
+            setShow(false)
+        }, delay*1000)
+
+        return () => {
+            clearTimeout(timeId)
+        }
+    }, []);
+
+    if (!show) {
+        return null;
+    }
+
+    return <div className={styles.text}> {text}</div>;
+}
